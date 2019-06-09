d4e0945cd97ec80766c44d14e117a0ed018a662a
Chance Hudson <jchancehud@gmail.com>
2019-06-08 23:15:13 -0500

    Name change, cli

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const { exec } = require('child_process')
+
+exec(`
+  cd android && ./gradlew assembleRelease
+`, (err, stdout, stderr) => {
+  console.log(stdout, stderr)
+})
